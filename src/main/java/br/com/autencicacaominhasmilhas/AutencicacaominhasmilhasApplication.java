8f97390c90c32574879a08e663dc80e7ab592d09
Joaomgsa <joaomgsa@gmail.com>
2022-08-02 17:41:57 -0300

    subindo primeiro projeto

---
+++ src/main/java/br/com/autencicacaominhasmilhas/AutencicacaominhasmilhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.autencicacaominhasmilhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutencicacaominhasmilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutencicacaominhasmilhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autencicacaominhasmilhas/AutencicacaominhasmilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autencicacaominhasmilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutencicacaominhasmilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

01f9cfac03013d66c6a131299f74847de1cc22e1
Joaomgsa <joaomgsa@gmail.com>
2022-08-02 18:09:47 -0300

    repositorio user e consulta simples listando todos usuarios

---
+++ src/main/java/br/com/autencicacaominhasmilhas/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.autencicacaominhasmilhas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.autencicacaominhasmilhas.model.UserModel;
+
+public interface UserRepository extends JpaRepository<UserModel, Integer>{
+
+	public Optional<UserModel> findByLogin(String login);
+}

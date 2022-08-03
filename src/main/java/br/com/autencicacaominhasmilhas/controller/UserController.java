9798b50ffda02b44764c1375d2da90ccbd4d1385
Joaomgsa <joaomgsa@gmail.com>
2022-08-03 09:48:09 -0300

    metodo de criacao usuarios

--- src/main/java/br/com/autencicacaominhasmilhas/controller/UserController.java
+++ src/main/java/br/com/autencicacaominhasmilhas/controller/UserController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class UserController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
-	@GetMapping("/save")
+	@PostMapping("/save")
 	public ResponseEntity<UserModel> save(@RequestBody UserModel user) {
 		return ResponseEntity.ok(repository.save(user));
 	}

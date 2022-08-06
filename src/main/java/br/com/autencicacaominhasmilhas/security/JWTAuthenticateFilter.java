97db37c0b8080624d8da706425168bb2d19416cc
Joaomgsa <joaomgsa@gmail.com>
2022-08-06 06:37:37 -0300

    tratando excecoes

--- src/main/java/br/com/autencicacaominhasmilhas/security/JWTAuthenticateFilter.java
+++ src/main/java/br/com/autencicacaominhasmilhas/security/JWTAuthenticateFilter.java
@@ -1,9 +1,20 @@
 package br.com.autencicacaominhasmilhas.security;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.autencicacaominhasmilhas.model.UserModel;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,7 +30,20 @@ public class JWTAuthenticateFilter extends UsernamePasswordAuthenticationFilter{
 	@Override 
 	public Authentication attemptAuthentication(HttpServletRequest request,
 												HttpServletResponse response) throws AuthenticationException{
+		try {
+			UserModel user = new ObjectMapper()
+					.readValue(request.getInputStream(), UserModel.class);
+		 
+			return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+				   user.getLogin(),
+				   user.getPassword(),
+				   new ArrayList<>()
+			));
 		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("Falha ao Autenticar o usuário",e);
+		}
 	}
 	
 }

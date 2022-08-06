c36fdd6a7ea0f776be35f2ef88192ca4499689c9
Joaomgsa <joaomgsa@gmail.com>
2022-08-06 06:59:04 -0300

    finalizando geracao token

--- src/main/java/br/com/autencicacaominhasmilhas/security/JWTAuthenticateFilter.java
+++ src/main/java/br/com/autencicacaominhasmilhas/security/JWTAuthenticateFilter.java
@@ -2,6 +2,7 @@ package br.com.autencicacaominhasmilhas.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -14,12 +15,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.autencicacaominhasmilhas.data.UserDetailsData;
 import br.com.autencicacaominhasmilhas.model.UserModel;
 
 public class JWTAuthenticateFilter extends UsernamePasswordAuthenticationFilter{
+	
+	public static final int TOKEN_EXPIRACAO = 600_000;
+	public static final String TOKEN_SENHA = "506bfc31-45cd-4086-bcc9-7511cdab7a22";
 	
 	private final AuthenticationManager authenticationManager;
 	
@@ -54,6 +60,13 @@ public class JWTAuthenticateFilter extends UsernamePasswordAuthenticationFilter{
 		
 		UserDetailsData userData = (UserDetailsData) authResult.getPrincipal();
 		
+		String token = JWT.create().
+				withSubject(userData.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + TOKEN_EXPIRACAO))
+				.sign(Algorithm.HMAC512(TOKEN_SENHA));
+		
+		response.getWriter().write(token);
+		response.getWriter().flush();
 	}
 	
 }

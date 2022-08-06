2e18ad6d5e8f5b0355be8104a03f0711c5d5478c
Joaomgsa <joaomgsa@gmail.com>
2022-08-05 22:16:47 -0300

    iniciando geracao de token jwt

---
+++ src/main/java/br/com/autencicacaominhasmilhas/security/JWTAuthenticateFilter.java
@@ -0,0 +1,8 @@
+package br.com.autencicacaominhasmilhas.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+public class JWTAuthenticateFilter extends UsernamePasswordAuthenticationFilter{
+
+}

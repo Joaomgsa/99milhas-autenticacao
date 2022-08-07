035d8330a80314701f448161b028ec272d9fc0e7
Joaomgsa <joaomgsa@gmail.com>
2022-08-07 18:10:44 -0300

    filtro da requisicao

--- src/main/java/br/com/autencicacaominhasmilhas/security/JWTValidateFilter.java
+++ src/main/java/br/com/autencicacaominhasmilhas/security/JWTValidateFilter.java
@@ -1,5 +1,12 @@
 package br.com.autencicacaominhasmilhas.security;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -7,7 +14,14 @@ public class JWTValidateFilter extends BasicAuthenticationFilter{
 
 	public JWTValidateFilter(AuthenticationManager authenticationManager) {
 		super(authenticationManager);
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain chain) throws IOException, ServletException{
+		
 	}
 
 }

bd630befcfcc1a4e419937ab7b037c024a010332
Joaomgsa <joaomgsa@gmail.com>
2022-08-08 06:48:15 -0300

    finalizando validacao da autenticacao

--- src/main/java/br/com/autencicacaominhasmilhas/security/JWTValidateFilter.java
+++ src/main/java/br/com/autencicacaominhasmilhas/security/JWTValidateFilter.java
@@ -1,6 +1,7 @@
 package br.com.autencicacaominhasmilhas.security;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,9 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.autencicacaominhasmilhas.security.JWTAuthenticateFilter;
+
 public class JWTValidateFilter extends BasicAuthenticationFilter{
+	
+	public static final String HEADER_ATRIBUTO = "Authorization";
+	public static final String ATRIBUTO_PREFIXO = "Bearer ";
 
 	public JWTValidateFilter(AuthenticationManager authenticationManager) {
 		super(authenticationManager);
@@ -22,6 +33,38 @@ public class JWTValidateFilter extends BasicAuthenticationFilter{
 									HttpServletResponse response,
 									FilterChain chain) throws IOException, ServletException{
 		
+		String atributo = request.getHeader(HEADER_ATRIBUTO); 
+		
+		if (atributo == null) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		if (!atributo.startsWith(ATRIBUTO_PREFIXO)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		String token = atributo.replace(ATRIBUTO_PREFIXO, "");
+		
+		UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(token);
+	
+		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+		chain.doFilter(request, response);
 	}
-
+		
+	private UsernamePasswordAuthenticationToken getAuthenticationToken(String token) {
+		
+		String user = JWT.require(Algorithm.HMAC512(JWTAuthenticateFilter.TOKEN_SENHA))
+				.build()
+				.verify(token)
+				.getSubject();
+				
+		if (user == null) {
+			return null;
+		}
+		
+		return new UsernamePasswordAuthenticationToken(user,null, new ArrayList<>());
+	}
+	
 }
